d4e4f8d94baa42a8a9bfb49e6662fc5422956018
clbrunet <brunet.clement5@gmail.com>
2021-03-02 11:01:35 +0100

    refactor(built_in): t_cmd support for echo

--- srcs/built_in/echo.c
+++ srcs/built_in/echo.c
@@ -6,29 +6,33 @@
 /*   By: mlebrun <mlebrun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 11:25:09 by mlebrun           #+#    #+#             */
-/*   Updated: 2021/03/01 06:22:39 by clbrunet         ###   ########.fr       */
+/*   Updated: 2021/03/06 12:13:49 by clbrunet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h" 
 #include "ft.h"
 
-/* args: temporary variable(need parsing): arguments and -n option */
-
 int	echo(t_cmd const *cmd)
 {
-	int		n_op = 1;
-	int		i;
+	int		n_flag;
+	char	**iter;
 
-	(void)cmd;
-	i = 0;
-	while (cmd->args[i] != NULL)
+	iter = cmd->args;
+	if (ft_strcmp(*iter, "-n") == 0)
 	{
-		ft_putstr_fd(1, cmd->args[i]);
-		ft_putchar_fd(1, ' ');
-		i++;
+		iter++;
+		n_flag = 1;
 	}
-	if (n_op)
+	else
+		n_flag = 0;
+	while (*iter != NULL)
+	{
+		ft_putstr_fd(1, *iter);
+		ft_putchar_fd(1, ' ');
+		iter++;
+	}
+	if (!n_flag)
 		ft_putchar_fd(1, '\n');
 	return (1);
 }
