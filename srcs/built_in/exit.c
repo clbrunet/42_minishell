6de624b3941c5d6ee7d18e1199bc613652b0f6af
clbrunet <brunet.clement5@gmail.com>
2021-03-21 09:57:08 +0100

    fix(exit): trim space in args[1]

--- srcs/built_in/exit.c
+++ srcs/built_in/exit.c
@@ -6,38 +6,45 @@
 /*   By: clbrunet <clbrunet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 09:09:02 by clbrunet          #+#    #+#             */
-/*   Updated: 2021/03/21 07:51:15 by clbrunet         ###   ########.fr       */
+/*   Updated: 2021/03/21 09:54:56 by clbrunet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h"
 #include "ft.h"
 
-int	exit_cmd(t_cmd const *cmd, char **envp_ptr[], int last_exit_code)
+static int	exit_with_args(char **args)
 {
 	int		i;
 
+	i = 0;
+	while (args[1][i] == ' ')
+		i++;
+	if (args[1][i] == '-' || args[1][i] == '+')
+		i++;
+	while (ft_isdigit(args[1][i]))
+		i++;
+	while (args[1][i] == ' ')
+		i++;
+	if (args[1][i])
+	{
+		ft_putstr_fd(2, "minishell: exit: ");
+		ft_putstr_fd(2, args[1]);
+		ft_putstr_fd(2, ": numeric argument required\n");
+		return (2);
+	}
+	if (args[2])
+	{
+		ft_putstr_fd(2, "minishell: exit: too many arguments\n");
+		return (-1);
+	}
+	return ((unsigned char)ft_atoi(args[1]));
+}
+
+int			exit_cmd(t_cmd const *cmd, char **envp_ptr[], int last_exit_code)
+{
 	(void)envp_ptr;
 	if (cmd->args[1])
-	{
-		i = 0;
-		if (cmd->args[1][0] == '-' || cmd->args[1][0] == '+')
-			i = 1;
-		while (ft_isdigit(cmd->args[1][i]))
-			i++;
-		if (cmd->args[1][i])
-		{
-			ft_putstr_fd(2, "minishell: exit: ");
-			ft_putstr_fd(2, cmd->args[1]);
-			ft_putstr_fd(2, ": numeric argument required\n");
-			return (2);
-		}
-		if (cmd->args[2])
-		{
-			ft_putstr_fd(2, "minishell: exit: too many arguments\n");
-			return (-1);
-		}
-		return ((unsigned char)ft_atoi(cmd->args[1]));
-	}
+		return (exit_with_args(cmd->args));
 	return (last_exit_code);
 }
